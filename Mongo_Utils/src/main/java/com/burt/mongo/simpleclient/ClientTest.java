4b31f03a9ef42bd36c1a4d35b48fbf9499449670
Shaun Machado <shaun@Shauns-MacBook-Pro.local>
2020-02-15 22:07:52 -0500

    Ongoing Updates

--- Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
+++ Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
@@ -10,7 +10,8 @@ import java.util.List;
 public class ClientTest {
     public static void main(String[] args) {
         MongoClient mongoClient = MongoClients.create();
-        populateCollection(mongoClient, "test", "test_col");
+//        populateCollection(mongoClient, "test", "test_col");
+//        populateCollectionMany(mongoClient, "test", "test_col");
         List<String> colList = getCollection(mongoClient, "test", "test_col");
         mongoClient.close();
         System.out.println("END");
@@ -29,13 +30,47 @@ public class ClientTest {
 
     }
 
+    private static void populateCollectionMany(MongoClient mongoClient, String dataBaseStr, String collectionStr) {
+        MongoDatabase database = mongoClient.getDatabase(dataBaseStr);
+        MongoCollection<Document> testCol = database.getCollection(collectionStr);
+        Document document = new Document("name", "John Doe")
+                .append("contact", new Document("phone", "001-444-0141")
+                        .append("email", "john.doe@example.com")
+                        .append("location", Arrays.asList(-177.92502, 110.8279556)))
+                .append("stars", 4)
+                .append("food", Arrays.asList("Burger", "Pizza", "Taco"));
+        testCol.insertOne(document);
+        document = new Document("name", "Jane Moe")
+                .append("contact", new Document("phone", "001-444-0142")
+                        .append("email", "jane.moe@example.com")
+                        .append("location", Arrays.asList(-178.92502, 110.8279556)))
+                .append("stars", 3)
+                .append("food", Arrays.asList("Pasta", "Salad", "Coffee"));
+        testCol.insertOne(document);
+        document = new Document("name", "Sam Loe")
+                .append("contact", new Document("phone", "001-444-0143")
+                        .append("email", "sam.loe@example.com")
+                        .append("location", Arrays.asList(-179.92502, 110.8279556)))
+                .append("stars", 4)
+                .append("food", Arrays.asList("Apple", "Dates", "Milk"));
+        testCol.insertOne(document);        document = new Document("name", "Sunny Joe")
+                .append("contact", new Document("phone", "001-444-0144")
+                        .append("email", "sunny.joe@example.com")
+                        .append("location", Arrays.asList(-180.92502, 110.8279556)))
+                .append("stars", 5)
+                .append("food", Arrays.asList("Fish", "Beef", "Wine"));
+        testCol.insertOne(document);
+    }
+
     public static List<String> getCollection(MongoClient mc, String db, String col){
         List<String> colList = new ArrayList<>();
         MongoDatabase mdb = mc.getDatabase(db);
         MongoCollection<Document> collection = mdb.getCollection(col);
         MongoCursor<Document> cursor = collection.find().iterator();
         try{
-            colList.add(cursor.next().toJson());
+            while(cursor.hasNext()){
+                colList.add(cursor.next().toJson());
+            }
         }finally {
             cursor.close();
         }
