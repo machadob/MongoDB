eb13ed718f27d75f443eda9b839db4ca0f5c147b
machadob <burton.machado@gmail.com>
2020-02-15 21:19:02 -0500

    Update ClientTest.java

--- Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
+++ Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
@@ -11,20 +11,25 @@ import java.util.Arrays;
 public class ClientTest {
     public static void main(String[] args) {
         MongoClient mongoClient = MongoClients.create();
-        MongoDatabase database = mongoClient.getDatabase("test");
-        MongoCollection<Document> testCol = database.getCollection("test_col");
+        populateCollection(mongoClient, "test", "test_col");
+        List<String> colList = getCollection(mongoClient, "test", "test_col");
+        System.out.println("END");
+    }
+
+    private static void populateCollection(MongoClient mongoClient, String dataBaseStr, String collectionStr) {
+        MongoDatabase database = mongoClient.getDatabase(dataBaseStr);
+        MongoCollection<Document> testCol = database.getCollection(collectionStr);
         Document document = new Document("name", "Udipi")
                 .append("contact", new Document("phone", "001-444-0149")
                         .append("email", "udipi@example.com")
                         .append("location", Arrays.asList(-177.92502, 110.8279556)))
                 .append("stars", 4)
                 .append("categories", Arrays.asList("Food", "Restaurant", "Fast Food"));
-
         testCol.insertOne(document);
-        System.out.println("END");
+
     }
-    
-        public static List<String> getCollection(MongoClient mc, String db, String col){
+
+    public static List<String> getCollection(MongoClient mc, String db, String col){
         List<String> colList = new ArrayList<>();
         MongoDatabase mdb = mc.getDatabase(db);
         MongoCollection<Document> collection = mdb.getCollection(col);
