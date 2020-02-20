06d520b134bd310c7bf2317067064958955e8e4d
Shaun Machado <shaun@Shauns-MacBook-Pro.local>
2020-02-19 22:16:07 -0500

    OGU

--- Mongo_Utils/src/main/java/com/burt/mongo/changestream/ChangeStreamTest.java
+++ Mongo_Utils/src/main/java/com/burt/mongo/changestream/ChangeStreamTest.java
@@ -22,12 +22,17 @@ public class ChangeStreamTest {
         MongoCursor<ChangeStreamDocument<Document>> cursor = testCol.watch().iterator();
         ChangeStreamDocument<Document> next = null;
         while(true){
-            next = cursor.next();
-            if(next.getOperationType() == OperationType.INSERT || next.getOperationType() == OperationType.UPDATE){
-                if(next.getFullDocument() != null){
-                    System.out.println("Doc is : " + next.getFullDocument().toJson());
+            if(cursor.hasNext())
+            {
+                next = cursor.next();
+                if(next.getOperationType() == OperationType.INSERT || next.getOperationType() == OperationType.UPDATE){
+                    if(next.getFullDocument() != null){
+                        System.out.println("Doc is : " + next.getFullDocument().toJson());
+                    }
                 }
             }
         }
     }
 }
+
+// /usr/local/opt/mongodb-community/bin/mongod --dbpath /Users/shaun/0000_LEARN/MongoDB/data --logpath /Users/shaun/0000_LEARN/MongoDB/Logs/mlog_00.log  --replSet testrepl --noauth
--- Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
+++ Mongo_Utils/src/main/java/com/burt/mongo/simpleclient/ClientTest.java
@@ -1,26 +1,107 @@
 package com.burt.mongo.simpleclient;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import org.bson.Document;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ClientTest {
     public static void main(String[] args) {
         MongoClient mongoClient = MongoClients.create();
-        MongoDatabase database = mongoClient.getDatabase("test");
-        MongoCollection<Document> testCol = database.getCollection("test_col");
+        populateCollection(mongoClient, "test", "test_col");
+//        populateCollectionMany(mongoClient, "test", "test_col");
+        List<String> colList = getCollection(mongoClient, "test", "test_col");
+        mongoClient.close();
+        System.out.println("END");
+    }
+
+    private static void populateCollection(MongoClient mongoClient, String dataBaseStr, String collectionStr) {
+        MongoDatabase database = mongoClient.getDatabase(dataBaseStr);
+        MongoCollection<Document> testCol = database.getCollection(collectionStr);
+        Document document = new Document("name", "Bdipi")
+                .append("contact", new Document("phone", "001-444-0149")
+                        .append("email", "Bdipi@example.com")
+                        .append("location", Arrays.asList(-177.92502, 110.8279556)))
+                .append("stars", 4)
+                .append("categories", Arrays.asList("Food", "Restaurant", "Fast Food"));
+        testCol.insertOne(document);
+
+    }
+
+    private static void populateCollection1(MongoClient mongoClient, String dataBaseStr, String collectionStr) {
+        MongoDatabase database = mongoClient.getDatabase(dataBaseStr);
+        MongoCollection<Document> testCol = database.getCollection(collectionStr);
         Document document = new Document("name", "Udipi")
                 .append("contact", new Document("phone", "001-444-0149")
                         .append("email", "udipi@example.com")
                         .append("location", Arrays.asList(-177.92502, 110.8279556)))
                 .append("stars", 4)
                 .append("categories", Arrays.asList("Food", "Restaurant", "Fast Food"));
-
         testCol.insertOne(document);
-        System.out.println("END");
+
+    }
+
+    private static void populateCollectionMany(MongoClient mongoClient, String dataBaseStr, String collectionStr) {
+        MongoDatabase database = mongoClient.getDatabase(dataBaseStr);
+        MongoCollection<Document> testCol = database.getCollection(collectionStr);
+        Document document = new Document("name", "John Doe")
+                .append("contact", new Document("phone", "001-444-0141")
+                        .append("email", "john.doe@example.com")
+                        .append("location", Arrays.asList(-177.92502, 110.8279556)))
+                .append("stars", 4)
+                .append("food", Arrays.asList("Burger", "Pizza", "Taco"));
+        testCol.insertOne(document);
+        document = new Document("name", "Jane Moe")
+                .append("contact", new Document("phone", "001-444-0142")
+                        .append("email", "jane.moe@example.com")
+                        .append("location", Arrays.asList(-178.92502, 110.8279556)))
+                .append("stars", 3)
+                .append("food", Arrays.asList("Pasta", "Salad", "Coffee"));
+        testCol.insertOne(document);
+        document = new Document("name", "Sam Loe")
+                .append("contact", new Document("phone", "001-444-0143")
+                        .append("email", "sam.loe@example.com")
+                        .append("location", Arrays.asList(-179.92502, 110.8279556)))
+                .append("stars", 4)
+                .append("food", Arrays.asList("Apple", "Dates", "Milk"));
+        testCol.insertOne(document);        document = new Document("name", "Sunny Joe")
+                .append("contact", new Document("phone", "001-444-0144")
+                        .append("email", "sunny.joe@example.com")
+                        .append("location", Arrays.asList(-180.92502, 110.8279556)))
+                .append("stars", 5)
+                .append("food", Arrays.asList("Fish", "Beef", "Wine"));
+        testCol.insertOne(document);
+    }
+
+    public static List<String> getCollection(MongoClient mc, String db, String col){
+        List<String> colList = new ArrayList<>();
+        MongoDatabase mdb = mc.getDatabase(db);
+        MongoCollection<Document> collection = mdb.getCollection(col);
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try{
+            while(cursor.hasNext()){
+                colList.add(cursor.next().toJson());
+            }
+        }finally {
+            cursor.close();
+        }
+        return colList;
+    }
+
+    public static List<String> getFilteredCollection(MongoClient mc, String db, String col){
+        List<String> colList = new ArrayList<>();
+        MongoDatabase mdb = mc.getDatabase(db);
+        MongoCollection<Document> collection = mdb.getCollection(col);
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try{
+            while(cursor.hasNext()){
+                colList.add(cursor.next().toJson());
+            }
+        }finally {
+            cursor.close();
+        }
+        return colList;
     }
 }
