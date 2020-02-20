53153d2645b7ac393f71f9ca435b7ef6b60016ed
Shaun Machado <shaun@Shauns-MacBook-Pro.local>
2020-02-19 23:14:29 -0500

    OGU

---
+++ Mongo_Utils/src/main/java/com/burt/mongo/changestream/ChangeStreamManager.java
@@ -0,0 +1,8 @@
+package com.burt.mongo.changestream;
+
+import java.util.concurrent.SynchronousQueue;
+
+public class ChangeStreamManager {
+    SynchronousQueue sq = new SynchronousQueue();
+    
+}
